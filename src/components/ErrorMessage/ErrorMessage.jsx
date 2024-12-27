68d7a8806e6c0124050ca668bdf247064f581fb9
Adriana <adrianat9099@gmail.com>
2024-12-27 19:34:53 +0200

    done

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,7 +1,8 @@
+import s from './ErrorMessage.module.css'
 const ErrorMessage = () => {
     return (
         <>
-        <h2>Something went wrong!!! Try again</h2>
+        <h2 className={s.error}>Something went wrong!!! Try again</h2>
         </>
     )
 }
